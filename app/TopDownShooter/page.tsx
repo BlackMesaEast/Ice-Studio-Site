4ef4babc2cf0dcacbf0885a346a65830744d9056
Niller <niclassylvest@gmail.com>
2026-01-11 00:00:28 +0100

    Playable game

--- app/TopDownShooter/page.tsx
+++ app/TopDownShooter/page.tsx
@@ -20,8 +20,26 @@ export default function TopDownShooter() {
           </div>
           <div>
             <h1 className="text-white">Spillet er mega awesome</h1>
+            <a
+              href="#Game"
+              className="border rounded px-4 py-2 bg-green-500 hover:bg-green-700 text-white inline-block text-center"
+            >
+              Spil Her
+            </a>
           </div>
         </div>
+        <section id="Game">
+          <div className="flex justify-center bg-zinc-900" role="application">
+            <div className="bg-white rounded-lg my-8 w-full bg-zinc-800 max-w-5xl p-4">
+              <iframe
+                src="/TopDownShooterWebGL/index.html"
+                className="w-full h-[675px] rounded-lg"
+                title="Zombie Plague WebGL"
+                allowFullScreen
+              ></iframe>
+            </div>
+          </div>
+        </section>
       </div>
     </div>
   );
